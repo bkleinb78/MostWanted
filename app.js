2ecd4ce0a76fe2ac8cbc7993129b74ffca0602fa
Brian Kleinberg <briankleinberg@MacBook-Pro.local>
2022-08-26 09:49:25 -0500

    completed project

--- app.js
+++ app.js
@@ -1,186 +1,362 @@
+"use strict"
 /*
-    Author: devCodeCamp
-    Description: Most Wanted Starter Code
+Build all of your functions for displaying and gathering information below (GUI).
 */
-//////////////////////////////////////////* Beginning Of Starter Code *//////////////////////////////////////////
 
-"use strict";
-//? Utilize the hotkey to hide block level comment documentation
-////* Mac: Press "CMD"+"K" and then "CMD"+"/"
-////* PC: Press "CTRL"+"K" and then "CTRL"+"/"
-
-/**
- * This is the main logic function being called in index.html.
- * It operates as the entry point for our entire application and allows
- * our user to decide whether to search by name or by traits.
- * @param {Array} people        A collection of person objects.
- */
-function app(people) {
-    // promptFor() is a custom function defined below that helps us prompt and validate input more easily
-    // Note that we are chaining the .toLowerCase() immediately after the promptFor returns its value
-    let searchType = promptFor(
-        "Do you know the name of the person you are looking for? Enter 'yes' or 'no'",
-        yesNo
-    ).toLowerCase();
-    let searchResults;
-    // Routes our application based on the user's input
-    switch (searchType) {
-        case "yes":
-            searchResults = searchByName(people);
-            break;
-        case "no":
-            //! TODO #4: Declare a searchByTraits (multiple traits) function //////////////////////////////////////////
-                //! TODO #4a: Provide option to search for single or multiple //////////////////////////////////////////
-            searchResults = searchByTraits(people);
-            break;
-        default:
-            // Re-initializes the app() if neither case was hit above. This is an instance of recursion.
-            app(people);
-            break;
-    }
-    // Calls the mainMenu() only AFTER we find the SINGLE PERSON
-    mainMenu(searchResults, people);
+// app is the function called to start the entire application
+function app(people){
+  let searchType = promptFor("Do you know the name of the person you are looking for? Enter 'yes' or 'no'", yesNo).toLowerCase();
+  let searchResults;
+  switch(searchType){
+    case 'yes':
+      searchResults = searchByName(people);
+      break;
+    case 'no':
+      // TODO: search by traits
+      searchByTraits(people);
+      break;
+      default:
+    app(people); // restart app
+      break;
+  }
+  
+  // Call the mainMenu function ONLY after you find the SINGLE person you are looking for
+  mainMenu(searchResults, people);
 }
-// End of app()
 
-/**
- * After finding a single person, we pass in the entire person-object that we found,
- * as well as the entire original dataset of people. We need people in order to find
- * descendants and other information that the user may want.
- * @param {Object[]} person     A singular object inside of an array.
- * @param {Array} people        A collection of person objects.
- * @returns {String}            The valid string input retrieved from the user.
- */
-function mainMenu(person, people) {
-    // A check to verify a person was found via searchByName() or searchByTrait()
-    if (!person[0]) {
-        alert("Could not find that individual.");
-        // Restarts app() from the very beginning
-        return app(people);
-    }
-    let displayOption = prompt(
-        `Found ${person[0].firstName} ${person[0].lastName}. Do you want to know their 'info', 'family', or 'descendants'?\nType the option you want or type 'restart' or 'quit'.`
-    );
-    // Routes our application based on the user's input
-    switch (displayOption) {
-        case "info":
-            //! TODO #1: Utilize the displayPerson function //////////////////////////////////////////
-            // HINT: Look for a person-object stringifier utility function to help
-            let personInfo = displayPerson(person[0]);
-            alert(personInfo);
-            break;
-        case "family":
-            //! TODO #2: Declare a findPersonFamily function //////////////////////////////////////////
-            // HINT: Look for a people-collection stringifier utility function to help
-            let personFamily = findPersonFamily(person[0], people);
-            alert(personFamily);
-            break;
-        case "descendants":
-            //! TODO #3: Declare a findPersonDescendants function //////////////////////////////////////////
-            // HINT: Review recursion lecture + demo for bonus user story
-            let personDescendants = findPersonDescendants(person[0], people);
-            alert(personDescendants);
-            break;
-        case "restart":
-            // Restart app() from the very beginning
-            app(people);
-            break;
-        case "quit":
-            // Stop application execution
-            return;
-        default:
-            // Prompt user again. Another instance of recursion
-            return mainMenu(person, people);
-    }
+// Menu function to call once you find who you are looking for
+function mainMenu(person, people){
+
+  /* Here we pass in the entire person object that we found in our search, as well as the entire original dataset of people. We need people in order to find descendants and other information that the user may want. */
+
+  if(!person){
+    alert("Could not find that individual.");
+    return app(people); // restart
+  }
+
+  let displayOption = prompt("Found " + person.firstName + " " + person.lastName + " . Do you want to know their 'info', 'family', or 'descendants'? Type the option you want or 'restart' or 'quit'");
+
+  switch(displayOption){
+    case "info":
+    // TODO: get person's info
+    displayPerson(person);
+    mainMenu(person,people);
+    break;
+    case "family":
+      displayFamilyInfo(person,people);
+      mainMenu(person,people);
+      break;
+    case "descendants":
+    displayPeople(displayDescendants(person,people));
+    mainMenu(person,people);
+    break;
+    case "restart":
+    app(people); // restart
+    break;
+    case "quit":
+    return; // stop execution
+    default:
+    return mainMenu(person, people); // ask again
+  }
 }
-// End of mainMenu()
 
-/**
- * This function is used when searching the people collection by
- * a person-object's firstName and lastName properties.
- * @param {Array} people        A collection of person objects.
- * @returns {Array}             An array containing the person-object (or empty array if no match)
- */
-function searchByName(people) {
-    let firstName = promptFor("What is the person's first name?", chars);
-    let lastName = promptFor("What is the person's last name?", chars);
+function searchByName(people){
+  let firstName = promptFor("What is the person's first name?", chars);
+  let lastName = promptFor("What is the person's last name?", chars);
 
-    // The foundPerson value will be of type Array. Recall that .filter() ALWAYS returns an array.
-    let foundPerson = people.filter(function (person) {
-        if (person.firstName === firstName && person.lastName === lastName) {
-            return true;
+  let foundPerson = people.filter(function(person){
+    if(person.firstName === firstName && person.lastName === lastName){
+      return true;
+    }
+    else{
+      return false;
+    }
+  })
+  // TODO: find the person using the name they entered
+  return foundPerson[0];
+}
+
+function selectTrait(){
+  let trait = promptFor("Which Trait would you like to search by?\n Gender, Height, Weight, Eye Color, Occupation, None", chars)
+  return trait;
+}
+
+
+function searchByTraits(people) {
+
+  let display;
+  let searchAgain = true;
+  
+
+  while(testing){
+    let trait = selectTrait().toLowerCase();
+  switch (trait) {
+    case "gender":
+      display = searchGender(people);
+      displayPeople(display);
+      break;
+    case "height":
+      display = searchHeight(people);
+      displayPeople(display);
+      break;
+    case "weight":
+      display = searchWeight(people);
+      displayPeople(display);
+      break;
+    case "eye color":
+      display = searchEyeColor(people);
+      displayPeople(display);
+      break;
+    case "occupation":
+      display = searchOccupation(people);
+      displayPeople(display);
+      break;
+    default:
+      break;
+  }
+  people = display;
+  searchAgain = multiCriteria(people);
+}
+  mainMenu(people[0]);
+}
+
+
+function multiCriteria(display){
+  if(display.length > 1){
+    alert("Please enter another search criteria!");
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+function searchGender(people) {
+  let input = promptFor("Is the person male or female?",chars);
+  let foundPeople = people.filter(function(person) {
+    if (person.gender === input) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  })
+  return foundPeople;
+//finds the person using the gender they entered
+}
+
+function searchHeight(people) {
+  let input = promptFor("Enter the person's height in inches?",chars);
+  let foundPeople = people.filter(function(person) {
+    if (person.height == input) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  })
+  return foundPeople;
+//finds the person using the gender they entered
+}
+
+function searchWeight(people) {
+  let input = promptFor("Enter the person's weight in pounds?",chars);
+  let foundPeople = people.filter(function(person) {
+    if (person.weight == input) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  })
+  return foundPeople;
+//finds the person using the gender they entered
+}
+
+function searchEyeColor(people) {
+  let input = promptFor("Enter the person's eye color?",chars);
+  let foundPeople = people.filter(function(person) {
+    if (person.eyeColor === input) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  })
+  return foundPeople;
+//finds the person using the gender they entered
+}
+
+function searchOccupation(people) {
+  let input = promptFor("Enter the person's occupation?",chars);
+  let foundPeople = people.filter(function(person) {
+    if (person.occupation === input) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  })
+  return foundPeople;
+//finds the person using the gender they entered
+}
+
+
+// alerts a list of people
+function displayPeople(people){
+  alert(people.map(function(person){
+    return person.firstName + " " + person.lastName;
+  }).join("\n"));
+}
+
+function displayPerson(person){
+  
+  // print all of the information about a person:
+  // height, weight, age, name, occupation, eye color.
+  let personInfo = "First Name: " + person.firstName + "\n";
+  personInfo += "Last Name: " + person.lastName + "\n";
+  personInfo += "Gender: " + person.gender + "\n";
+  personInfo += "DOB: " + person.dob + "\n";
+  personInfo += "Height: " + person.height + "\n";
+  personInfo += "Weight: " + person.weight + "\n";
+  personInfo += "Eye Color: " + person.eyeColor + "\n";
+  personInfo += "Occupation: " + person.occupation + "\n";
+
+
+  // TODO: finish getting the rest of the information to display
+  alert(personInfo);
+}
+
+var desList = [];
+var counter = 0;
+function displayDescendants(person, people) {
+
+  let foundPeople = people.filter(function (el) {
+    if (person.id === el.parents[0] || person.id === el.parents[1]) {
+      desList.push(el);
+      return true;
+    }
+    else {
+      return false;
+    }
+  })
+  for (var i = counter; i < desList.length; i++) {
+    counter++;
+    displayDescendants(desList[i], people);
+  }
+  return desList;
+}
+
+
+
+function findParents(person, people) {
+ let findParent;
+  if (person.parents.length == 0) {
+    return null;
+
+  }
+  else if (person.parents.length == 1) {
+
+      findParent = people.filter(function (el) {
+      if (person.parents[0] === el.id) {
+        return true;
+      }
+      else {
+        return false;
+      }
+    }
+    )}
+
+  else {
+    findParent = people.filter(function (el) {
+      if (person.parents[0] === el.id || person.parents[1] === el.id ) {
+        return true;
+      }
+      else {
+        return false;
+      }
+      })
+}
+  return findParent;
+}
+
+function findSpouse(person, people){
+let findSpouse = people.filter(function (el) {
+  if (person.currentSpouse === el.id){
+    return true;
+    
+  }
+  else{
+    return false;
+}
+})
+return findSpouse;
+}
+
+function findSiblings(person, people) {
+  for (let i = 0; i < person.parents.length; i++) {
+    if (person.parents[i] != undefined) {
+      var findSiblings = people.filter(function (el) {
+        if ((person.parents[i] === el.parents[0] || person.parents[i] === el.parents[1]) && person.id !== el.id) {
+          return true;
         }
-    });
-    return foundPerson;
+        else {
+          return false;
+        }
+      })
+    
+  }
+  
+  }
+  return findSiblings;
 }
-// End of searchByName()
 
-/**
- * This function will be useful for STRINGIFYING a collection of person-objects
- * first and last name properties in order to easily send the information
- * to the user in the form of an alert().
- * @param {Array} people        A collection of person objects.
- */
-function displayPeople(people) {
-    alert(
-        people
-            .map(function (person) {
-                return `${person.firstName} ${person.lastName}`;
-            })
-            .join("\n")
-    );
+function displayFamilyInfo(person, people) {
+  let print = "";
+  let parents = findParents(person,people);
+  let spouse = findSpouse(person, people);
+  let siblings = findSiblings(person, people);
+
+  if (parents != null){
+    parents.forEach(function(parent){
+      print +=  "Parent(s): " + parent.firstName + " " + parent.lastName + "\n";
+    })
+  }
+  else{
+    print += "Parentz: None\n";
+  }
+
+  if(spouse != undefined){
+    print += "Spousez:" + spouse[0].firstName + " " + spouse[0].lastName + "\n";
+  }
+  else{
+    print += "Spousez: None\n";
+  }
+if (siblings != null){
+  siblings.forEach(function(sibling){
+      print += "Siblings(s): " + sibling.firstName + " " + sibling.lastName + "\n";
+    })
 }
-// End of displayPeople()
-
-/**
- * This function will be useful for STRINGIFYING a person-object's properties
- * in order to easily send the information to the user in the form of an alert().
- * @param {Object} person       A singular object.
- */
-function displayPerson(person) {
-    let personInfo = `First Name: ${person.firstName}\n`;
-    personInfo += `Last Name: ${person.lastName}\n`;
-    //! TODO #1a: finish getting the rest of the information to display //////////////////////////////////////////
-    alert(personInfo);
+else{
+  print += "Siblingz: None";
 }
-// End of displayPerson()
-
-/**
- * This function's purpose is twofold:
- * First, to generate a prompt with the value passed in to the question parameter.
- * Second, to ensure the user input response has been validated.
- * @param {String} question     A string that will be passed into prompt().
- * @param {Function} valid      A callback function used to validate basic user input.
- * @returns {String}            The valid string input retrieved from the user.
- */
-function promptFor(question, valid) {
-    do {
-        var response = prompt(question).trim();
-    } while (!response || !valid(response));
-    return response;
+  alert(print);
 }
-// End of promptFor()
 
-/**
- * This helper function checks to see if the value passed into input is a "yes" or "no."
- * @param {String} input        A string that will be normalized via .toLowerCase().
- * @returns {Boolean}           The result of our condition evaluation.
- */
-function yesNo(input) {
-    return input.toLowerCase() === "yes" || input.toLowerCase() === "no";
+// function that prompts and validates user input
+function promptFor(question, valid){
+  do{
+    var response = prompt(question).trim();
+  } while(!response || !valid(response));
+  return response;
 }
-// End of yesNo()
 
-/**
- * This helper function operates as a default callback for promptFor's validation.
- * Feel free to modify this to suit your needs.
- * @param {String} input        A string.
- * @returns {Boolean}           Default validation -- no logic yet.
- */
-function chars(input) {
-    return true; // Default validation only
+// helper function to pass into promptFor to validate yes/no answers
+function yesNo(input){
+  return input.toLowerCase() == "yes" || input.toLowerCase() == "no";
 }
-// End of chars()
 
-//////////////////////////////////////////* End Of Starter Code *//////////////////////////////////////////
-// Any additional functions can be written below this line 👇. Happy Coding! 😁
+// helper function to pass in as default promptFor validation
+function chars(input){
+  return true; // default validation only
+}
